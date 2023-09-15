e13834ad1da9a753775330ffc621d1f7fe570305
Eric Marquez <ericscottmarquez@gmail.com>
2023-09-14 21:47:42 -0500

    function now gets to convert_from_bytes function in upload-convert-pdf

--- upload-convert-pdf/__init__.py
+++ upload-convert-pdf/__init__.py
@@ -5,38 +5,83 @@ from PIL import Image
 import io
 import pymongo
 import azure.functions as func
-from pdf2image import convert_from_path
+from pdf2image import convert_from_bytes
+import time
+import logging
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     try:
         # Step 0: Setup MongoDB Connection using the connection string from .env
         mongodb_cnx_str = os.environ["mongodb_atlas_cnx_str"]
         client = pymongo.MongoClient(mongodb_cnx_str)
-        db = client.get_database('your_database_name')  # replace with your database name
-        collection = db.get_collection('your_collection_name')  # replace with your collection name
+        db = client.get_database('pdf_summarizer')  # replace with your database name
+        collection = db.get_collection('pdf')  # replace with your collection name
+        # log the connection string to the console
+        logging.info(collection)
 
         # Step 1: Accept a PDF file and a unique userId
         pdf_file = req.files['pdf']
+        # log the pdf file
+        logging.info(pdf_file)
         user_id = req.form['userId']
 
+        # log the userId
+        logging.info(user_id)
+
         connection_str = os.environ["AZURE_STORAGE_CONNECTION_STRING"]
+        # log the connection string to the console
+        logging.info(connection_str)
         
         # Step 2: Connect to a storage blob container and create a new folder with userId
         blob_service_client = BlobServiceClient.from_connection_string(connection_str)
+        # log the blob service client to the console with a description of the message:
+        logging.info(f"Blob service client: {blob_service_client}")
+
         container_name = "pdfsummarizer"
-        folder_name = os.path.splitext(pdf_file.filename)[0] + f'_{user_id}/'
-        
-        images = convert_from_path(pdf_file)
+        folder_name = os.path.splitext(pdf_file.name)[0] + f'_{user_id}/'
+        # log the folder name and a description of the message:
+        logging.info(f"Folder name: {folder_name}")
+
+        time.sleep(5)  # Wait for 5 seconds to ensure that the container folder is created
+        try:
+            # Get the byte data from the FileStorage object
+            pdf_bytes = pdf_file.read()
+            pdf_file.seek(0)  # Reset the file pointer to the start
+            # Convert the byte data to images
+            images = convert_from_bytes(pdf_bytes)
+        except Exception as e:
+            # log the error message to the console with a description:
+            logging.info(f"Error at convert_from_bytes: {str(e)}")
+
         for page_num, image in enumerate(images):
-            image_io = io.BytesIO()
-            image.save(image_io, format='JPEG')
-            
-            blob_name = folder_name + f'{page_num}.jpeg'
-            blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_name)
-            blob_client.upload_blob(image_io.getvalue(), overwrite=True)
+            try:
+                image_io = io.BytesIO()
+                image.save(image_io, format='JPEG')
+            except Exception as e:
+                logging.exception(f"Error saving image to BytesIO at page {page_num}: {str(e)}")
+                continue  # Skip to next iteration if this step fails
+
+            try:
+                blob_name = folder_name + f'{page_num}.jpeg'
+            except Exception as e:
+                logging.exception(f"Error forming blob name at page {page_num}: {str(e)}")
+                continue  # Skip to next iteration if this step fails
+
+            try:
+                blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_name)
+            except Exception as e:
+                logging.exception(f"Error getting blob client at page {page_num}: {str(e)}")
+                continue  # Skip to next iteration if this step fails
+
+            try:
+                blob_client.upload_blob(image_io.getvalue(), overwrite=True)
+            except Exception as e:
+                logging.exception(f"Error uploading blob at page {page_num}: {str(e)}")
+                continue  # Skip to next iteration if this step fails
+
         
         # Step 5: Get the URL of the blob and save it in MongoDB
-        blob_url = f"https://{blob_service_client.account_name}.blob.core.windows.net/{container_name}/{folder_name}"
+        blob_url = f"https://pdfsummarizer.blob.core.windows.net/converted-pdfs/{folder_name}"
         collection.insert_one({"user_id": user_id, "name": folder_name, "blob_url": blob_url})
         
         # Step 6: Return a success status code
